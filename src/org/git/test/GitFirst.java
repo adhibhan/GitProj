06ba83bd8985a9667fcb896c621cbda9ba70d1d8
adhibhan <sargururohini@gmail.com>
2019-06-13 12:18:27 +0530

    this is the first commit

---
+++ src/org/git/test/GitFirst.java
@@ -0,0 +1,12 @@
+package org.git.test;
+
+public class GitFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the first Git");
+
+	}
+
+}
